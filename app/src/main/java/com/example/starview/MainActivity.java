e5884dcd8613b5abfe833117f55b541e0a347e43
JoshPorterDev <porterjosh417@gmail.com>
2021-05-10 13:17:10 -0500

    updated information screen

--- app/src/main/java/com/example/starview/MainActivity.java
+++ app/src/main/java/com/example/starview/MainActivity.java
@@ -3,6 +3,17 @@ package com.example.starview;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,5 +22,16 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setTheme(R.style.Theme_StarView);
         setContentView(R.layout.activity_main);
+
+        findViewById(R.id.submitGeographicalDataButton).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText cityStateEditText = findViewById(R.id.cityStateEditText);
+
+                // Test output
+                TextView testTextView = findViewById(R.id.testTextView);
+                testTextView.setText(cityStateEditText.getText());
+            }
+        });
     }
 }
